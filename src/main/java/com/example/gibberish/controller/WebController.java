b73d53e558a7129b583034b6b3b6dd04c45340ca
tiemotm <tiemo.timtschenko@gmx.de>
2021-12-11 14:47:31 +0100

    added controller

---
+++ src/main/java/com/example/gibberish/controller/WebController.java
@@ -0,0 +1,32 @@
+package com.example.gibberish.controller;
+
+import com.example.gibberish.Gibberish;
+import com.example.gibberish.repository.ProverbRepository;
+import com.example.gibberish.soundex.Soundex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WebController {
+
+    private ProverbRepository repository;
+    Soundex soundex = new Soundex();
+
+    @Autowired
+    WebController(ProverbRepository repository) {
+        this.repository = repository;
+    }
+
+    // Generate gibberish from string
+    @GetMapping("/gibber")
+    Gibberish gibber(@RequestParam(required = false) String text) {
+        if(text == null) {
+            text = repository.getRandom();
+        }
+        return new Gibberish(text, String.join(" ", soundex.encode(text)));
+    }
+}
---
+++ src/test/java/com/example/gibberish/controller/WebControllerTests.java
@@ -0,0 +1,63 @@
+package com.example.gibberish.controller;
+
+import com.example.gibberish.Gibberish;
+import com.example.gibberish.repository.ProverbRepository;
+import jdk.jfr.ContentType;
+import netscape.javascript.JSObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(WebController.class)
+public class WebControllerTests {
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProverbRepository repository;
+
+    @Autowired
+    public WebControllerTests(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void gibberShouldReturnRandomGibberishIfNoParams() throws Exception {
+        when(repository.getRandom()).thenReturn("foo").thenReturn("bar").thenReturn("foobar");
+        this.mockMvc.perform(get("/api/gibber"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("foo")));
+        this.mockMvc.perform(get("/api/gibber"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("bar")));
+        this.mockMvc.perform(get("/api/gibber"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("foobar")));
+    }
+
+    @Test
+    public void gibberShouldReturnJSON() throws Exception {
+        when(repository.getRandom()).thenReturn("foo");
+        this.mockMvc.perform(get("/api/gibber"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void gibberShouldReturnGibberishFromParameter() throws Exception {
+        this.mockMvc.perform(get("/api/gibber?text=foo"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("foo")));
+    }
+}
