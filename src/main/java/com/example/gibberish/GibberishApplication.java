34bd597c90f986199aa7024152659a98f2a1ef8e
tiemotm <tiemo.timtschenko@gmx.de>
2021-12-11 14:50:04 +0100

    added application

---
+++ src/main/java/com/example/gibberish/GibberishApplication.java
@@ -0,0 +1,13 @@
+package com.example.gibberish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibberishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibberishApplication.class, args);
+	}
+
+}
